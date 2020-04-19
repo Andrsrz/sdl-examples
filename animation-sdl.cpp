471f615353ae3e7281cf95d73a8eff97e089cbc3
Andres Ruiz <andrsruiz@gmail.com>
2020-04-19 10:22:12 -0500

    Change spaces to TABS

--- animation-sdl.cpp
+++ animation-sdl.cpp
@@ -20,74 +20,74 @@ const int MAX_SPEED = 6;
 
 void initButterflies(std::vector<Butterfly> butterflies, SDL_Surface* screen);
 void drawButterflies(std::vector<Butterfly> butterflies,
-                     SDL_Surface* screen,
-                     SDL_Surface* butterfly);
+						SDL_Surface* screen,
+						SDL_Surface* butterfly);
 void moveButterflies(std::vector<Butterfly> butterflies, SDL_Surface* screen);
 
 int main(){
-    SDL_Surface* screen;
-    SDL_Surface* butterfly;
-    SDL_Surface* background;
-    SDL_Rect src, dest;
-    int frames;
+	SDL_Surface* screen;
+	SDL_Surface* butterfly;
+	SDL_Surface* background;
+	SDL_Rect src, dest;
+	int frames;
 
-    // Initialize SDL's video system and check for erros.
-    if(SDL_Init(SDL_INIT_VIDEO) != 0){
-        std::cout << "Unable to initialize SDL: " << SDL_GetError() << std::endl;
-        return 1;
-    }
+	// Initialize SDL's video system and check for erros.
+	if(SDL_Init(SDL_INIT_VIDEO) != 0){
+		std::cout << "Unable to initialize SDL: " << SDL_GetError() << std::endl;
+		return 1;
+	}
 
-    // Make sure SDL_Quit gets called then the program exits!
-    atexit(SDL_Quit);
+	// Make sure SDL_Quit gets called then the program exits!
+	atexit(SDL_Quit);
 
-    // Attempt to set a 500x500 hicolor (16-bit) video mode.
-    screen = SDL_SetVideoMode(500, 500, 16, 0);
-    if(screen == NULL){
-        std::cout << "Unable to set video mode: " << SDL_GetError() << std::endl;
-        return 1;
-    }
+	// Attempt to set a 500x500 hicolor (16-bit) video mode.
+	screen = SDL_SetVideoMode(500, 500, 16, 0);
+	if(screen == NULL){
+		std::cout << "Unable to set video mode: " << SDL_GetError() << std::endl;
+		return 1;
+	}
 
-    // Load the bitmap file.
-    background = SDL_LoadBMP("background.bmp");
-    butterfly = SDL_LoadBMP("butterfly.bmp");
-    if((background == NULL) || (butterfly == NULL)){
-        std::cout << "Unable to load bitmap" << std::endl;
-        return 1;
-    }
+	// Load the bitmap file.
+	background = SDL_LoadBMP("background.bmp");
+	butterfly = SDL_LoadBMP("butterfly.bmp");
+	if((background == NULL) || (butterfly == NULL)){
+		std::cout << "Unable to load bitmap" << std::endl;
+		return 1;
+	}
 
-    /* Set the butterfly’s colorkey. */
-    SDL_SetColorKey(butterfly,
-                    SDL_SRCCOLORKEY,
-                    (Uint16) SDL_MapRGB(butterfly->format,
-                                        255, 255, 255));
+	/* Set the butterfly’s colorkey. */
+	SDL_SetColorKey(butterfly,
+					SDL_SRCCOLORKEY,
+					(Uint16) SDL_MapRGB(butterfly->format,
+										255, 255, 255));
 
-    /* vector needs to not be constant */
-    std::vector<Butterfly> butterflies {::NUM_BUTTERFLIES};
-    /* Initialize the butterfly position data. */
-    initButterflies(butterflies, screen);
+	/* vector needs to not be constant */
+	std::vector<Butterfly> butterflies {::NUM_BUTTERFLIES};
+	/* Initialize the butterfly position data. */
+	initButterflies(butterflies, screen);
 
-    /* Animate 300 frames (approximately 10 seconds). */
-    for(frames = 0; frames < 3000; frames++){
-        /* Draw the background image. */
-        src.x = 0;
-        src.y = 0;
-        src.w = background->w;
-        src.h = background->h;
-        dest = src;
-        SDL_BlitSurface(background, &src, screen, &dest);
-        /* Put the butterflies on the screen. */
-        drawButterflies(butterflies, screen, butterfly);
-        /* Ask SDL to update the entire screen. */
-        SDL_UpdateRect(screen, 0, 0, 0, 0);
-        /* Move the butterflies for the next frame. */
-        moveButterflies(butterflies, screen);
-    }
+	/* Animate 300 frames (approximately 10 seconds). */
+	for(frames = 0; frames < 3000; frames++){
+		/* Draw the background image. */
+		src.x = 0;
+		src.y = 0;
+		src.w = background->w;
+		src.h = background->h;
+		dest = src;
+		SDL_BlitSurface(background, &src, screen, &dest);
+		/* Put the butterflies on the screen. */
+		drawButterflies(butterflies, screen, butterfly);
+		/* Ask SDL to update the entire screen. */
+		SDL_UpdateRect(screen, 0, 0, 0, 0);
+		/* Move the butterflies for the next frame. */
+		moveButterflies(butterflies, screen);
+	}
 
-    /* Free the memory that was allocated to the bitmap. */
-    SDL_FreeSurface(background);
-    SDL_FreeSurface(butterfly);
+	/* Free the memory that was allocated to the bitmap. */
+	SDL_FreeSurface(background);
+	SDL_FreeSurface(butterfly);
 
-    return 0;
+	return 0;
 }
 
 /*
@@ -95,47 +95,47 @@ int main(){
  * starting position and direction.
  */
 void initButterflies(std::vector<Butterfly> butterflies, SDL_Surface* screen){
-    for(int i = 0; i < ::NUM_BUTTERFLIES; i++){
-        butterflies[i].setX( rand() % screen->w );
-        butterflies[i].setY( rand() % screen->h );
-        butterflies[i].setDx( (rand() % (::MAX_SPEED * ::TWO)) - ::MAX_SPEED );
-        butterflies[i].setDy( (rand() % (::MAX_SPEED * ::TWO)) - ::MAX_SPEED );
-    }
+	for(int i = 0; i < ::NUM_BUTTERFLIES; i++){
+		butterflies[i].setX( rand() % screen->w );
+		butterflies[i].setY( rand() % screen->h );
+		butterflies[i].setDx( (rand() % (::MAX_SPEED * ::TWO)) - ::MAX_SPEED );
+		butterflies[i].setDy( (rand() % (::MAX_SPEED * ::TWO)) - ::MAX_SPEED );
+	}
 }
 
 /* This routine draws each butterfly to the screen surface. */
 void drawButterflies(std::vector<Butterfly> butterflies,
-                     SDL_Surface* screen,
-                     SDL_Surface* butterfly){
-    SDL_Rect src, dest;
-    for(int i = 0; i < ::NUM_BUTTERFLIES; i++){
-        src.x = 0;
-        src.y = 0;
-        src.w = butterfly->w;
-        src.h = butterfly->h;
-        /*
-         * The butterfly’s position specifies its center.
-         * We subtract half of its width and height to get
-         * its upper left corner.
-        */
-        dest.x = butterflies[i].getX() - butterfly->w / TWO;
-        dest.y = butterflies[i].getY() - butterfly->h / TWO;
-        dest.w = butterfly->w;
-        dest.h = butterfly->h;
-        SDL_BlitSurface(butterfly, &src, screen, &dest);
-    }
+						SDL_Surface* screen,
+						SDL_Surface* butterfly){
+	SDL_Rect src, dest;
+	for(int i = 0; i < ::NUM_BUTTERFLIES; i++){
+		src.x = 0;
+		src.y = 0;
+		src.w = butterfly->w;
+		src.h = butterfly->h;
+		/*
+		 * The butterfly’s position specifies its center.
+		 * We subtract half of its width and height to get
+		 * its upper left corner.
+		*/
+		dest.x = butterflies[i].getX() - butterfly->w / TWO;
+		dest.y = butterflies[i].getY() - butterfly->h / TWO;
+		dest.w = butterfly->w;
+		dest.h = butterfly->h;
+		SDL_BlitSurface(butterfly, &src, screen, &dest);
+	}
 }
 
 /* This routine moves each butterfly by its motion vector. */
 void moveButterflies(std::vector<Butterfly> butterflies, SDL_Surface* screen){
-    for(int i = 0; i < ::NUM_BUTTERFLIES; i++){
-       /* Move the butterfly by its motion vector. */
-        butterflies[i].setX( butterflies[i].getX() + butterflies[i].getDx() );
-        butterflies[i].setY( butterflies[i].getY() + butterflies[i].getDy() );
-        /* Turn the butterfliesaround if it hits the edge of the screen. */
-        if (butterflies[i].getX() < 0 || butterflies[i].getX() > screen->w - 1)
-            butterflies[i].setDx( -butterflies[i].getDx() );
-        if (butterflies[i].getY() < 0 || butterflies[i].getY() > screen->h - 1)
-            butterflies[i].setDy( -butterflies[i].getDy() );
-    }
+	for(int i = 0; i < ::NUM_BUTTERFLIES; i++){
+		/* Move the butterfly by its motion vector. */
+		butterflies[i].setX( butterflies[i].getX() + butterflies[i].getDx() );
+		butterflies[i].setY( butterflies[i].getY() + butterflies[i].getDy() );
+		/* Turn the butterfliesaround if it hits the edge of the screen. */
+		if (butterflies[i].getX() < 0 || butterflies[i].getX() > screen->w - 1)
+			butterflies[i].setDx( -butterflies[i].getDx() );
+		if (butterflies[i].getY() < 0 || butterflies[i].getY() > screen->h - 1)
+			butterflies[i].setDy( -butterflies[i].getDy() );
+	}
 }
--- butterfly.hpp
+++ butterfly.hpp
@@ -1,19 +1,18 @@
 /* Butterfly class. */
 
 class Butterfly {
-    public:
-        Butterfly();
-        ~Butterfly();
-        void setX(int posX);
-        int getX();
-        void setY(int posY);
-        int getY();
-        void setDx(int movX);
-        int getDx();
-        void setDy(int movY);
-        int getDy();
-    private:
-        int x, y; /* Screen position */
-        int dx, dy; /* Movement vector */
-
+	public:
+		Butterfly();
+		~Butterfly();
+		void setX(int posX);
+		int getX();
+		void setY(int posY);
+		int getY();
+		void setDx(int movX);
+		int getDx();
+		void setDy(int movY);
+		int getDy();
+	private:
+		int x, y; /* Screen position */
+		int dx, dy; /* Movement vector */
 };
